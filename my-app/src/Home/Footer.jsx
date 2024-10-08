7cf6d6404bf16b9ec12d321d2ddfa185aeffe788
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-10-08 13:15:39 +0300

    improved interface

--- my-app/src/Home/Footer.jsx
+++ my-app/src/Home/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
 
+
 const Footer = () => {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -29,14 +30,31 @@ const Footer = () => {
 
   return (
     <div>
+
       {isVisible && (
-        <button onClick={scrollToTop} className="backToTopBtn">          
+        <button onClick={scrollToTop} className="backToTopBtn">
           <img src="https://img.icons8.com/ios/50/000000/up.png" alt="" />
         </button>
       )}
+
       <footer className='myFooter'>
+        <div id='contacts' className='contacts'>
+          <h3>Follow Us</h3>          
+          <div className="social-icons">
+            <a href="https://t.me" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios/50/000000/telegram-app.png" alt="Telegram" />
+            </a>
+            <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios/50/000000/discord-logo.png" alt="Discord" />
+            </a>
+            <a href="https://www.twitch.tv/" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios/50/000000/twitch.png" alt="Twitch" />
+            </a>
+          </div>          
+        </div>
         <p>© 2024 Denmap. All rights reserved.</p>
       </footer>
+
     </div>
 
   );
