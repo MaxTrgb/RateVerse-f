e3a25c14625205b70438715bbfef82333975b0d2
Denys <33674663+BlackOrangee@users.noreply.github.com>
2024-10-22 14:42:36 +0300

    change user page

--- my-app/src/Home/UserPage.jsx
+++ my-app/src/Home/UserPage.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './UserPage.css';
 import Header from './Header';
 import Footer from './Footer';
-import { message } from 'antd';
+import { Rate, message } from 'antd';
+import PostGrid from '../Games/postGrid';
 
 const UserPage = () => {
+    const { id } = useParams();
     const [userInfo, setUserInfo] = useState({
         name: '',
         email: '',
         password: '',
         image: '',
+        rating: 0.0,
     });
 
     const userId = localStorage.getItem('userId');
@@ -17,7 +21,7 @@ const UserPage = () => {
     useEffect(() => {
         const fetchUserInfo = async () => {
             try {
-                const response = await fetch(`/api/v1/user/${userId}`);
+                const response = await fetch(`/api/v1/user/${id == null ? userId : id}`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch user info');
                 }
@@ -28,10 +32,10 @@ const UserPage = () => {
             }
         };
 
-        if (userId) {
+        if (userId || id) {
             fetchUserInfo();
         }
-    }, [userId]);
+    }, [id, userId]);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -73,44 +77,63 @@ const UserPage = () => {
                 <main >
                     <h1>User Information</h1>
                     <div className="user-info">
-                        <img
-                            src={userInfo.image}
-                            alt={`${userInfo.name}'s avatar`}
-                            className="ava"
-                        />
-
-
-                        <form onSubmit={handleSubmit} className="user-form">
-                            <div className="form-group">
-                                <label htmlFor="name">Name:</label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    value={userInfo.name}
-                                    onChange={handleChange}
-                                    required
+                        <div>
+                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <img
+                                    src={userInfo.image}
+                                    alt={`${userInfo.name}'s avatar`}
+                                    className="ava"
                                 />
+
+                                <div className='rating'>
+                                    <Rate
+                                        disabled
+                                        value={userInfo.rating}
+                                    />
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="password">New Password:</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    value={userInfo.password}
-                                    onChange={handleChange}
-                                    required
-                                />
+
+                            <div>
+                                {id == null || userId === id
+                                    ? <form onSubmit={handleSubmit} className="user-form">
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name:</label>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                value={userInfo.name}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="password">New Password:</label>
+                                            <input
+                                                type="password"
+                                                id="password"
+                                                name="password"
+                                                value={userInfo.password}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                        </div>
+                                        <button type="submit" className="submit-button">Update Info</button>
+                                    </form>
+                                    :
+                                    <h1>{userInfo.name}</h1>
+                                }
                             </div>
-                            <button type="submit" className="submit-button">Update Info</button>
-                        </form>
-                        <button onClick={handleLogout} className="logout-button">Logout</button>
+                        </div>
+                        <div>
+                            {id == null || userId === id ? <button onClick={handleLogout} className="logout-button">Logout</button> : null}
+                        </div>
                     </div>
 
 
                 </main>
             </div>
+            <PostGrid userId={id} userName={userInfo.name} />
             <Footer />
         </div>
     );
